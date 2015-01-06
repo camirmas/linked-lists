aebf1ffb0d8935ae54b323f94d3790c622366a1e
Cameron Irmas <cjirmas@gmail.com>
2015-01-06 15:46:36 -0800

    test refactor

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -18,16 +18,18 @@ end
 describe LinkedList do
   let(:my_list) {LinkedList.new(5)}
 
-  it "has a head that is a node" do
-    expect(my_list.head.class).to be == Node
-  end
+  describe "#initialize" do
+    it "has a head that is a node" do
+      expect(my_list.head.class).to be == Node
+    end
 
-  it "has a tail that is a node" do
-    expect(my_list.tail.class).to be == Node
-  end
+    it "has a tail that is a node" do
+      expect(my_list.tail.class).to be == Node
+    end
 
-  it "has a tail that is a node" do
-    expect(my_list.tail.class).to be == Node
+    it "has a tail that is a node" do
+      expect(my_list.tail.class).to be == Node
+    end
   end
 
   it "can have items appended to it" do
@@ -53,51 +55,42 @@ describe LinkedList do
     expect(my_list.length).to be == 3
   end
 
-  it "finds items" do
-    my_list.append("hello")
-    found_node = my_list.find("hello")
+  describe "#find" do
+    it "finds items" do
+      my_list.append("hello")
+      found_node = my_list.find("hello")
 
-    expect(found_node.value).to be == "hello"
+      expect(found_node.value).to be == "hello"
+    end
+
+    it "returns 'not found' when items are not found" do
+      found_node = my_list.find("yes")
+
+      expect(found_node).to be nil
+    end
   end
 
-  it "returns 'not found' when items are not found" do
-    my_list.append("hello")
-    found_node = my_list.find("yes")
+  describe "#delete" do
+    it "deletes items" do
+      my_list.append("hello")
+      my_list.delete(5)
 
-    expect(found_node).to be nil
+      expect(my_list.find(5)).to be nil
+    end
+
+    it "cannot delete items that don't exist" do
+      expect(my_list.delete("what?")).to be == "not found"
+    end
   end
 
-  it "deletes items" do
-    my_list.append("hello")
-    my_list.delete(5)
+  describe "#insert_after" do
+    it "inserts items after a given item" do
+      my_list.insert_after(5, "yes")
+      expect(my_list.tail.value).to be == "yes"
+    end
 
-    expect(my_list.find(5)).to be nil
-  end
-
-  it "cannot delete items that don't exist!" do
-    my_list.append("hello")
-    my_list.delete("what?")
-
-    expect(my_list.delete("what?")).to be == "not found"
-  end
-
-  it "inserts items after a given item" do
-    my_list.insert_after(5, "yes")
-    expect(my_list.tail.value).to be == "yes"
-  end
-
-  it "does not insert items after a given item that doesn't exist" do
-    expect(my_list.insert_after("hello", "yes")).to be == "not found!"
-  end
-
-  it "deletes items that are in the linked list" do
-    my_list.append("hello")
-    my_list.delete("hello")
-
-    expect(my_list.find("hello")).to be == nil
-  end
-
-  it "returns 'not found' when the item to delete is not in the linked list" do
-    expect(my_list.delete("hello")).to be == "not found"
+    it "returns 'not found! when the given item doesn't exist" do
+      expect(my_list.insert_after("hello", "yes")).to be == "not found!"
+    end
   end
 end
